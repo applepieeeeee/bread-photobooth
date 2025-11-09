a13cf3ea390bbe7d1bf412ba68e1af6972eeff91
clair <clair.theshruyb@gmail.com>
2025-11-08 19:56:44 -0500

    added state and view variables for js

---
+++ script.js
@@ -0,0 +1,20 @@
+// variables 
+
+const VIEWS = {
+    START: 'view-start',
+    CAPTURE: 'view-capture'
+}
+
+const STATE = {
+    captured: [],
+    videoStream: null,
+    isCapturing: false
+}
+
+const CANVAS_SIZE = { width: 600, height: 450 };
+
+// elements
+
+
+// functions
+
